ceb4fa9ded2fb7c0f4775df239a47cb357dee965
Mukesh Kumar <mukeshbcc56@gmail.com>
2024-06-14 21:55:13 +1000

    Enable saving wrong answers

---
+++ TavelApp/Controllers/WronganswerController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using TavelApp.Models;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+
+namespace TavelApp.Controllers;
+
+
+[Route("api/[controller]")]
+public class WronganswerController : Controller
+{
+    
+    private readonly IConfiguration _configuration; // For reading connection string
+
+    public WronganswerController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // GET
+    [HttpPost("postwronganswer")] // Use a descriptive endpoint
+    public IActionResult Index([FromBody] WrongAnswer wrongAnswer)
+    {
+        
+        var connectionString = _configuration.GetConnectionString("AzurePostgresConnection");
+
+        // 2. Npgsql Connection Object
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        if (wrongAnswer.email == null)
+        {
+            return BadRequest("please signin to save score");
+        }
+
+        // 3. SQL Command Preparation
+        var command = new NpgsqlCommand("INSERT INTO user_wronganswers (email, question_number) VALUES (@email, @question_number)", connection);
+        command.Parameters.AddWithValue("@email", wrongAnswer.email);
+        command.Parameters.AddWithValue("@question_number", Int32.Parse(wrongAnswer.question_number));
+
+        var rowsAffected = command.ExecuteNonQuery();
+
+        if (rowsAffected > 0)
+        {
+            return Ok("User Exists");
+        }
+        
+        return BadRequest("User registration failed"); 
+    }
+}
---
+++ TavelApp/Models/WrongAnswer.cs
@@ -0,0 +1,9 @@
+namespace TavelApp.Models;
+
+public class WrongAnswer
+{
+    public string email { get; set; }
+    public string question_number { get; set; }
+
+
+}
