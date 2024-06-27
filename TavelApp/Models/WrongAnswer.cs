e8c7ed2cb407197a9c37b17b66a838c438c5367f
Mukesh Kumar <mukeshbcc56@gmail.com>
2024-06-27 14:12:13 +1000

    Add new column id

--- TavelApp/Models/WrongAnswer.cs
+++ TavelApp/Models/WrongAnswer.cs
@@ -2,6 +2,8 @@ namespace TavelApp.Models;
 
 public class WrongAnswer
 {
+    
+    public int id { get; set; }
     public string email { get; set; }
     public string question_number { get; set; }
 
